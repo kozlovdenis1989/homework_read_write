be88c1ac1576a84a985bb18678fe392b1a3fe9f0
Denis Kozlov <yadenisko1989@gmail.com>
2024-12-29 19:09:45 +0300

    homework_2, add get_shop_list_by_dishes

--- book.py
+++ book.py
@@ -2,41 +2,67 @@ from pprint import pprint
 
 
 class Book():
-    def __init__(self):
-        self.cook_book = {}
-        self.string_list = []
+    def __init__(self, file: str = None):
+        self.file = file
+        self.string_list = self.read_write(self.file)
+        self.cook_book = self.conv_to_dict(self.string_list)
 
     # Функция для чтения и записи(перезаписи) файла. Данные при чтении сохраняются в переменную string_list
     # в виде списка строк, знаки преноса удаляются.
     # Данные при записи(перезаписи) сохраняются в файл.
     # Реализованы только три режима.
-    def read_write(self, file: str, mode: str, context: str = None):
+    def read_write(self, file: str = None, mode: str = 'r', content: str = None):
+        '''
+        Метод для чтения и записи(перезаписи) файла. Данные при чтении сохраняются в переменную string_list
+        в виде списка строк, знаки преноса удаляются. Вызывается при инициализации класса.
+        Данные при записи(перезаписи) сохраняются в файл.
+        Реализованы только три режима.
+
+        :param file: относительный путь к файлу, необязательный параметр, по умолчанию None
+        :param mode: режим 'r', 'a', 'w'; необязательный параметр, по умолчанию 'r'
+        :param content: контент для обновления(дополнения), необязательный параметр, по умолчанию None
+        :return: list
+        '''
+        if file == None:
+            return []
         with open(file, mode) as f:
           match mode:
-            case 'r':   self.string_list = [line.strip() for line in f ]
+            case 'r':
+                self.string_list = [line.strip() for line in f ] + ['']
+                return self.string_list
             case 'w':
-                if context != None:
-                    f.write(context)
+                if content != None:
+                    f.write(content)
                     print(f'Файл {file} удачно презаписан')
                 else:
                     print(f'{file} остался без изменений. Передайте что-нибудь в переменную context')
             case 'a':
-                if context != None:
-                    f.write('\n' + context)
+                if content != None:
+                    f.write(content)
                     print(f'В файл {file} добавлена новая запись')
                 else:
                     print(f'{file} остался без изменений. Передайте что-нибудь в переменную context')
             case _:   raise ValueError("Другие режимы пока не предусмотренны. Необходимо передать 'r', 'w', 'a' ")
 
-    # Функция преобразования списка из переменной string_list в словарь со списками словарей.
-    def conv_to_dict(self):
+
+    def conv_to_dict(self, string_list: list):
+        '''
+        Метод преобразования списка из переменной string_list. Возвращает словарь со списками словарей, также записывает
+        это значение в переменную cook_book. Вызывается при инициализации класса.
+        При изменении файла методом read_write, необходимо вызвать тот же метод в режиме mode = 'r', для обновления
+        string_list.
+
+        :param string_list: Список из файла
+        :return: dict
+        '''
         start = 0
         new_list = []
+        cook_book = {}
 
-        for index in range(len(self.string_list)):
+        for index in range(len(string_list)):
             stop = index
-            if self.string_list[index] == '':
-                new_list.append(self.string_list[start : stop])
+            if string_list[index] == '':
+                new_list.append(string_list[start : stop])
                 start = stop + 1
 
         for ingredients_list  in new_list:
@@ -45,18 +71,61 @@ class Book():
 
             for ingredients in ingredients_list[2:]:
                 ingredient = ingredients.split('|')
-                dish_ingredient.append({'ingredient_name': ingredient[0], 'quantity': ingredient[1], 'measure': ingredient[2]})
+                dish_ingredient.append({'ingredient_name': ingredient[0], 'quantity': int(ingredient[1]), 'measure': ingredient[2]})
 
-            self.cook_book[dish] = dish_ingredient
+            cook_book[dish] = dish_ingredient
+
+        self.cook_book = cook_book
+        return self.cook_book
 
 
+    def get_shop_list_by_dishes(self, dishes: list, person_count: int):
+        '''
+        Мето дподсчета всех ингредиентов, блюд, переданных в переменной dishes, на всех персон person count.
+        Все значения dishes должны быть в переменной cook_book, иначе получим ошибку.
+        Возвращает словарь со всеми ингредиентами на указанное количество персон.
+
+        :param dishes: список со стоками (назнавия блюд)
+        :param person_count: число персон
+        :return: dict
+        '''
+        ingredients = {}
+
+        for dish in dishes:
+            if dish not in self.cook_book:
+                raise  ValueError(f'В cook_book отсутствует {dish}')
+            else:
+                for ingredient in self.cook_book[dish]:
+                    if ingredient['ingredient_name'] not in ingredients:
+                        ingredients[ingredient['ingredient_name']] = {
+                            'quantity': ingredient['quantity'] * person_count,
+                                                          'measure': ingredient['measure']}
+                    else:
+                        ingredients[ingredient['ingredient_name']] = {
+                            'quantity': ingredients[ingredient['ingredient_name']]['quantity'] + ingredient['quantity'] * person_count,
+                                                          'measure': ingredient['measure']}
+
+        return ingredients
 
 
+# Контент для добавления
+content = '\nКапучино\n3\nКофе | 10 | г\nВода | 20 | мл\nМолоко | 40 | мл\n'
 
+# Создаем экземпляр, "recipes.txt" необязательный параметр. Чтение происходит автоматически
+# и записывается в переменную string_list.
 book = Book()
-book.read_write("recipes.txt", "r", '')
+# book.read_write("recipes.txt", "a", content)
 
-book.conv_to_dict()
+# Метод чтения и записи. Если при создании кса Book не передавать относительный путь, то можно это сделать здесь.
+book.read_write("recipes.txt", "r")
+
+# Задание 1. Метод преобразования данных из файла в вид, описанный в задании 1. Результат записывается в cook.book.
+book.conv_to_dict(book.string_list)
 pprint(book.cook_book)
 
+# Задание 2. Метод подсчета общего количества ингредиентов по переданным параметрам блюд и персон.
+pprint(book.get_shop_list_by_dishes(['Омлет', 'Запеченный картофель', 'Утка по-пекински', 'Капучино'], 2))
 
+# print(Book.read_write.__doc__)
+# print(Book.conv_to_dict.__doc__)
+# print(Book.get_shop_list_by_dishes.__doc__)
